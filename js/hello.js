de6fb1ea9e2f52342a96a87cfc9d9b15013e1b54
Haiden Arulappan <you@example.com>
2021-11-19 18:30:29 +0530

    update

--- js/hello.js
+++
@@ -1,9 +0,0 @@
-// document.write("hello world");
-
-
-// alert("this is an alert");
-
-function submitHandler(){
-    const FirstName=document.getElementById(FirstName);
-    console.log("First Name is :" + Firstname.value);
-}
