61c0c7597f0a60badae76b87e2cc76af798207c5
Haiden Arulappan <you@example.com>
2021-12-08 14:03:18 +0530

    added

--- asserts/javascript/form.js
+++ asserts/javascript/form.js
@@ -1,18 +1,61 @@
-  
-function check(){
-     event.preventDefault();
+let fullarray = [];
 
-    let a = document.getElementById("create").value;
-    let b = document.getElementById("conform").value;
+function pageload() {
 
-    if(a==b){
-     console.log("Matching");
-     window.location.replace("../index.html");
+    const statge = JSON.parse(localStorage.getItem("statge"));
+    console.log(statge)
+    if (statge) {
+        fullarray = statge;
     }
-    else{
+
+}
+
+function store() {
+    event.preventDefault();
+
+    let name = document.getElementById("username").value;
+    let email = document.getElementById("gmail").value;
+    let phnumber = document.getElementById("number").value;
+    let createpassword = document.getElementById("create").value;
+    let confirmpassword = document.getElementById("conform").value;
+
+    const customer_details = {
+
+        "Name": name,
+        "Gmail": email,
+        "Phone_number": phnumber,
+        "Create": createpassword,
+        "Confirm": confirmpassword,
+    }
+
+    const checking = checkpassword();
+
+
+    if (checking) {
+        fullarray.push(customer_details);
+        localStorage.setItem("statge", JSON.stringify(fullarray))
+        window.location.href = "../index.html";
+    }
+    else {
         console.log("Not matching");
         alert("Error")
     }
+};
 
-    
-}
+function checkpassword() {
+    let createpassword = document.getElementById("create").value;
+    let confirmpassword = document.getElementById("conform").value;
+
+    if (createpassword == confirmpassword) {
+        console.log("password Matched")
+        return true;
+
+    }
+    else {
+        return false;
+    }
+};
+
+
+
+pageload();
