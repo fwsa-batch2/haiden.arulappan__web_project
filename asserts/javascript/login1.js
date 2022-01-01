e17618573e47433a22c878a33c17a9a4c19ab41a
Haiden Arulappan <you@example.com>
2022-01-02 00:30:08 +0530

    update

---
+++ asserts/javascript/login1.js
@@ -0,0 +1,42 @@
+function login() {
+    event.preventDefault();
+    let email = document.getElementById("gmail").value;
+    let password = document.getElementById("secret").value;
+    const isexist = isuserexist(email, password);
+    if (isexist) {
+        localStorage.setItem("loggedinuser", email);
+        window.location.href = "./../../Pages/himform.17.html"
+    }
+    else {
+        alert("invalid email or password");
+        return null;
+    }
+}
+function isuserexist(paramemail, parampassword) {
+    let isexist = false;
+    const userdetails = JSON.parse(localStorage.getItem("userList"));
+    const len = userdetails.length;
+    console.log(len);
+
+    for (i = 0; i < len; i++) {
+        const user = userdetails[i];
+        const useremail = user.Gmail;
+        const userpassword = user.Create;
+        if (useremail === paramemail && userpassword === parampassword) {
+            isexist = true;
+            break;
+        }
+    }
+    return isexist;
+}
+function checkbox() {
+    let check = document.getElementById("checking");
+    if (check.checked) {
+        document.getElementById("secret").type = "Text";
+    }
+    else {
+        document.getElementById("secret").type = "password";
+    }
+}
+
+
