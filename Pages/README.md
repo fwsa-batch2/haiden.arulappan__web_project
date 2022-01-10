3b2352692f3e80d3b4ac2c843df0eae2009b8966
Haiden Arulappan <you@example.com>
2022-01-10 11:55:33 +0530

    added new files

--- Pages/README.md
+++ Pages/README.md
@@ -1,1 +1,30 @@
-# html-project-template
+1.Register Page:
+
+   Mail cannot be repeat.
+   All inputs must be filled.
+   create password and confirm password should be same.
+   phone number should contain ten numbers.
+
+2.login page:
+    
+    The user should register otherwise it will show error in the  login page.
+    The mail and password should be same which is registered.
+
+3.Forget page:
+
+    If the user forget the password,they want to enter the email.
+    The OTP will sent to the entered email.
+    If the OTP is wrong it will not move to the next page.
+    They should enter the correct OTP and it will move to the next page.
+
+4.Review page:
+    
+    The user is wished to review they can review.
+    And their review will shown in the our reviews.
+
+5.My Bookings:
+
+    The user can see their booking hotels.
+    And with their details.
+
+
---
+++ asserts/javascript/addinghotel.js
@@ -0,0 +1,33 @@
+let hoteldetails = [];
+
+function pageload() {
+    const gettingDetails = JSON.parse(localStorage.getItem("details"));
+
+    console.log(gettingDetails);
+    if (gettingDetails != null) {
+        hoteldetails = gettingDetails;
+    }
+}
+
+
+function submitHandler(event) {
+    event.preventDefault();
+    let hotelName = document.getElementById("hotelname").value;
+    let ownername = document.getElementById("ownername").value;
+    let Phone_number = document.getElementById("ph_number").value;
+    let Image_link = document.getElementById("hotelimage").value;
+
+    const newhotelDetails = {
+        "hotelname": hotelName,
+        "ownername": ownername,
+        "ph_number": Phone_number,
+        "Image": Image_link
+    };
+
+    console.log(hoteldetails);
+    hoteldetails.push(newhotelDetails);
+    localStorage.setItem("details", JSON.stringify(hoteldetails));
+    window.location.href = ""
+
+}
+pageload();
