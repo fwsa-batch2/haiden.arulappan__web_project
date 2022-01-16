2cd8fea7d47dbb9679a8de1334ce377b8dcc8280
Haiden Arulappan <you@example.com>
2022-01-17 00:30:04 +0530

    designed

--- asserts/javascript/listing_hotels.js
+++ asserts/javascript/listing_hotels.js
@@ -9,10 +9,13 @@ function onPageLoad() {
 }
 onPageLoad();
 function listinghotels(item) {
-    document.getElementById("adding_hotels").innerHTML += `<div>Hotel Name:${item.hotelname}</div>
-    <div>Owner Name:${item.ownername}</div>
-   <div> Phone Number:${item.ph_number}</div>
-    <div>Hotel Picture:<img src="${item.Image}" alt="" style="height:100px">
-    </div>`
+    document.getElementById("adding_hotels").innerHTML += `<div style="margin-top:8%;font-size:1vw;opacity:.6;color:#3c1700">Hotel Name</div>
+    <div style="color:#3c1700">${item.hotelname}</div><br>
+    <div style="font-size:1vw;opacity:.6;color:#3c1700">Owner Name</div>
+    <div style="color:#3c1700">${item.ownername}</div><br>
+   <div style="font-size:1vw;opacity:.6;color:#3c1700"> Phone Number</div>
+   <div style="color:#3c1700">${item.ph_number}</div>
+    <div><img src="${item.Image}" alt="" style="height:32vh;margin-left:42%;margin-top:-32%;border-radius:5px">
+    </div><br><br><hr style="opacity:.6">`
 
 }
